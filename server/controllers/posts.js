3cd47adf55783ea300e82ed1710f2981262a197f
Forum-123 <69541616+Forum-123@users.noreply.github.com>
2021-10-20 12:27:11 +0100

    Add route functions

---
+++ server/controllers/posts.js
@@ -0,0 +1,33 @@
+const Post = require('../models/post');
+
+// Index function - returns all posts
+async function index(req, res) {
+    try {
+        const allPosts = await Post.all;
+        res.status(200).json({ posts: allPosts });
+    } catch (err) {
+        res.status(500).json({ err });
+    };
+};
+
+// Show function - returns one post by id
+async function show(req, res) {
+    try {
+        const post = await Post.findById(parseInt(req.params.id));
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(404).json({ err });
+    };
+};
+
+// Create function - returns newly created book
+async function create(req, res) {
+    try {
+        const post = await Post.create(req.body);
+        res.status(201).json(post);
+    } catch (err) {
+        res.status(422).json({ err });
+    };
+};
+
+module.exports = { index, show, create };
