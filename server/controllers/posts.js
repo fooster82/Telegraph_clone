459d123cc3a8a087ad70cfdb3a03499c43d2bc2f
Forum-123 <69541616+Forum-123@users.noreply.github.com>
2021-10-21 15:00:43 +0100

    Implement Mongo's ObjectId and fix createPost function

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -13,7 +13,7 @@ async function index(req, res) {
 // Show function - returns one post by id
 async function show(req, res) {
     try {
-        const post = await Post.findById(parseInt(req.params.id));
+        const post = await Post.findById(req.params.id);
         res.status(200).json(post);
     } catch (err) {
         res.status(404).json({ err });
